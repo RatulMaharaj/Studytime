0798151181e07104344bf9b92e99b205fb4c8235
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-07-09 02:36:42 +0200

    Hide sign-up button on smaller screens using css only

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,9 @@
-import React, {useState, useEffect} from "react";
-import { Link } from "gatsby";
-import Logo from "./utility/Logo";
-import "./Header.css";
+import React from "react"
+import { Link } from "gatsby"
+import Logo from "./utility/Logo"
+import "./Header.css"
 
 function Header() {
-
-  const breakpoint = 620;
-  const [width, setWidth] = useState(0)
-
-  useEffect(() => {
-    setWidth(window.innerWidth)
-  }, [])
-
   return (
     <div className="Header">
       <div className="Logo">
@@ -20,17 +12,11 @@ function Header() {
         </Link>
       </div>
       <div className="Admin">
-        {width < breakpoint ? (
-          <button className="btn-purple">Login</button>
-        ) : (
-          <>
-            <button className="btn-purple">Login</button>
-            <button className="btn-purple">Sign Up</button>
-          </>
-        )}
+        <button className="btn-purple">Login</button>
+        <button className="btn-purple">Sign Up</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
