bac77445061eb0b3a2644c6ac970dd62b0dbc3de
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-08-05 22:50:44 +0200

    Reordered Subjects

--- src/components/subjects/Subjects.js
+++ src/components/subjects/Subjects.js
@@ -12,11 +12,11 @@ function Content() {
       </div>
       <div className="Card-wrapper">
         <Card
-          link='/apps?subject=communications&learn=0&practice=0&test=0'
-          icon={<GiMicrophone className="icon" />}
-          heading="Communications"
-          tags={["N211"]}
-          description="Communications Exam Preparation."
+          link='/apps?subject=models&practice=0&test=0'
+          icon={<GiLifeBar className="icon" />}
+          heading="Risk Modelling and Survival Analysis"
+          tags={["A212", "CS2"]}
+          description="Flashcards, Past Exam Questions and MCQ's based on the A212 content (combined A202 and A204)."
         />
         <Card
           link='/apps?subject=contingencies&practice=0&test=0'
@@ -26,11 +26,11 @@ function Content() {
           description="Past Exam Questions and MCQ's based on the content of A213 (commonly referred to as CT5)."
         />
         <Card
-          link='/apps?subject=models&practice=0&test=0'
-          icon={<GiLifeBar className="icon" />}
-          heading="Risk Modelling and Survival Analysis"
-          tags={["A212", "CS2"]}
-          description="Flashcards, Past Exam Questions and MCQ's based on the A212 content (combined A202 and A204)."
+          link='/apps?subject=communications&learn=0&practice=0&test=0'
+          icon={<GiMicrophone className="icon" />}
+          heading="Communications"
+          tags={["N211"]}
+          description="Communications Exam Preparation."
         />
       </div>
     </div>
