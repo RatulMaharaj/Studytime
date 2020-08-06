61de0a95bca975ac67fb0b9b8ce2b1a65b25250a
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-08-06 14:48:18 +0200

    added shortcuts to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, {useEffect} from "react"
 import Header from "../components/Header"
 import Subjects from "../components/subjects/Subjects"
 import Footer from "../components/Footer"
 import SEO from "../components/seo"
 
 function Index() {
+  const onKeydown = e => {
+    if(e.key === '1'){
+      e.preventDefault()
+      document.getElementById("RiskModels").click()
+    } else if (e.key === '2'){
+      e.preventDefault()
+      document.getElementById("Contis").click()
+    } else if (e.key === '3'){
+      e.preventDefault()
+      document.getElementById("Comms").click()
+    }
+    }
+  useEffect(() => {
+    document.addEventListener("keydown", onKeydown)
+    return () => document.removeEventListener("keydown", onKeydown)
+  })
   return (
     <div id="root">
       <SEO title="Home" />
